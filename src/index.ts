ce7ebe5f91dd16de8b040a1cce811f912dd810fa
SP_Penguin <abrcdf1023@gmail.com>
2020-09-27 16:36:21 +0800

    feat first example done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+const num1 = document.getElementById("num1") as HTMLInputElement
+const num2 = document.getElementById("num2") as HTMLInputElement
+const addBtn = document.getElementById('addBtn') as HTMLButtonElement
+
 function add(a: number, b: number) {
   return a + b
 }
+
+addBtn.addEventListener("click", () => {
+  console.log(add(+num1.value, +num2.value))
+})
 
 // You Should Not Passed!
 // add("a", "b")
